ee98a44a75e26569e89c070d5513d029e8707b1b
ShikaseDaiki <daiki19977252@gmail.com>
2024-07-23 08:13:07 +0900

    NextAuthのセッションを設定した。

---
+++ src/app/lib/next-auth/options.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import prisma from "../prisma";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return { ...session, user: { ...session.user, id: user.id } };
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
